758d4a139499708c51278f4ec60df9938c57b886
Emily-Campbell23 <emcampbel@hartford.edu>
2023-11-01 16:11:48 -0400

    added default matrix without color

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,12 +2,29 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
-    String size;
+    int size;
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Please enter the size of the matrix: ");
+    System.out.print("Please enter the size of the matrix using a positive number: ");
 
-    size = scanner.nextLine();
+    size = scanner.nextInt();
+
+    System.out.println("Your matrix is " + size + " X " + size);
+    System.out.println("Now printing matrix with default values...");
+
+
+    int[][] matrix = new int[size][size];
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[row].length; col++) {
+        matrix[row][col] = (0) + col;
+      }
+    }
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[row].length; col++) {
+        System.out.print (matrix[0][0] + "\t");
+      }
+      System.out.println();
     scanner.close();
+    }
   }
 }
