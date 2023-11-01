86da61eb30e92a1c66601df77ef08be9d8fde656
Emily-Campbell23 <emcampbel@hartford.edu>
2023-11-01 15:56:36 -0400

    user input

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,12 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    String size;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Please enter the size of the matrix: ");
+
+    size = scanner.nextLine();
+    scanner.close();
   }
 }
