dae0ac2c1e490238aae5710efff87bfaa245ebd2
Emily-Campbell23 <emcampbel@hartford.edu>
2023-11-08 16:08:29 -0500

    update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,19 @@
 public class Matrix {
-  
+  int size;
+  public int matrix() {
+    int[][] matrix = new int[size][size];{
+    for (int height = 0; height < matrix.length; height++) {
+      for (int width = 0; width < matrix[height].length; width++) {
+        matrix[height][width] = (0) + width;
+      }
+    }
+    for (int height = 0; height < matrix.length; height++) {
+      for (int width = 0; width < matrix[height].length; width++) {
+        System.out.print(matrix[0][0] + "\t");
+      }
+      System.out.println();
+      }
+    }
+    return matrix();
+  }
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,29 +2,15 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
-    int size;
+    Matrix size1;
+    size1 = new Matrix();
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Please enter the size of the matrix using a positive number: ");
-
-    size = scanner.nextInt();
-
-    System.out.println("Your matrix is " + size + " X " + size);
+    System.out.println("Please enter the size of the matrix using a positive number: ");
+    //size1 = scanner.nextLine();
+    System.out.println("Your matrix is " + size1 + " X " + size1);
     System.out.println("Now printing matrix with default values...");
-
-
-    int[][] matrix = new int[size][size];
-    for (int row = 0; row < matrix.length; row++) {
-      for (int col = 0; col < matrix[row].length; col++) {
-        matrix[row][col] = (0) + col;
-      }
-    }
-    for (int row = 0; row < matrix.length; row++) {
-      for (int col = 0; col < matrix[row].length; col++) {
-        System.out.print (matrix[0][0] + "\t");
-      }
-      System.out.println();
     scanner.close();
     }
   }
-}
+
