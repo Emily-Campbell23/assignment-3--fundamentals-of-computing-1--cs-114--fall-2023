1460c648cbf57277af19f2ecd05e93b37b3051ac
Emily-Campbell23 <emcampbel@hartford.edu>
2023-11-15 13:47:57 -0500

    flipped entire third matrix

--- Matrix.java
+++ Matrix.java
@@ -2,18 +2,16 @@ public class Matrix {
     int size;
     int[][] matrix;
 
-    // Constructor should have the same name as the class (capitalized)
     public Matrix(int size) {
         this.size = size;
         this.matrix = new int[size][size];
 
-        // Indicate that we are now populating the matrix
         System.out.println("Now populating matrix...");
 
         // Populate the first matrix with all 0s and highlight the diagonal from lower left to upper right
         for (int height = 0; height < matrix.length; height++) {
             for (int width = 0; width < matrix[height].length; width++) {
-                if (height + width == size - 1) {
+                if (width == size - height - 1) {
                     // Highlight the diagonal with light green background
                     System.out.print("\u001B[42m" + matrix[height][width] + "\t\u001B[0m");
                 } else {
@@ -41,15 +39,30 @@ public class Matrix {
         System.out.println("\nThird Matrix:");
 
         // Populate the third matrix without repeats, incrementing by 1
-        count = 1;
-        for (int height = 0; height < matrix.length; height++) {
-            for (int width = 0; width < matrix[height].length; width++) {
-                matrix[height][width] = count++;
+        count = size * size; // Start from the maximum value and decrement
+        for (int height = matrix.length - 1; height >= 0; height--) {
+            for (int width = matrix[height].length - 1; width >= 0; width--) {
+                matrix[height][width] = count--;
             }
         }
 
-        // Print the third matrix with the diagonal highlighted from lower left to upper right
-        printMatrixFromLowerLeftToUpperRight();
+        // Print the third matrix with the highlighted diagonal unchanged and other numbers flipped
+        printFlippedMatrixFromLowerLeftToUpperRight();
+    }
+
+    // Method to print the matrix with the diagonal highlighted from lower left to upper right
+    private void printFlippedMatrixFromLowerLeftToUpperRight() {
+        for (int height = matrix.length - 1; height >= 0; height--) {
+            for (int width = matrix[height].length - 1; width >= 0; width--) {
+                // Highlight the diagonal from lower left to upper right with light green background
+                if (width == size - height - 1) {
+                    System.out.print("\u001B[42m" + matrix[height][width] + "\t\u001B[0m");
+                } else {
+                    System.out.print(matrix[height][width] + "\t");
+                }
+            }
+            System.out.println();
+        }
     }
 
     // Method to print the matrix with the diagonal highlighted from lower left to upper right
@@ -65,5 +78,10 @@ public class Matrix {
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        // Example usage with user input 4
+        Matrix matrix = new Matrix(4);
     }
 }
