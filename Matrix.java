8c5049f39d37606e03a0c66947f0d22e15151275
Emily-Campbell23 <emcampbel@hartford.edu>
2023-11-14 22:04:02 -0500

    highlighted on the diagonal

--- Matrix.java
+++ Matrix.java
@@ -1,19 +1,26 @@
+// Matrix.java
 public class Matrix {
-  int size;
-  public int matrix() {
-    int[][] matrix = new int[size][size];{
-    for (int height = 0; height < matrix.length; height++) {
-      for (int width = 0; width < matrix[height].length; width++) {
-        matrix[height][width] = (0) + width;
-      }
+    int size;
+    int[][] matrix;
+
+    // Constructor should have the same name as the class (capitalized)
+    public Matrix(int size) {
+        this.size = size;
+        this.matrix = new int[size][size];
+
+        // Populate the matrix with 0s, highlighting the diagonal with light green
+        for (int height = 0; height < matrix.length; height++) {
+            for (int width = 0; width < matrix[height].length; width++) {
+                if (height + width == size - 1) {
+                    // Highlight the diagonal by setting diagonal elements to 0 with light green background
+                    matrix[height][width] = 0;
+                    System.out.print("\u001B[42m" + matrix[height][width] + "\t\u001B[0m");
+                } else {
+                    matrix[height][width] = 0;
+                    System.out.print(matrix[height][width] + "\t");
+                }
+            }
+            System.out.println();
+        }
     }
-    for (int height = 0; height < matrix.length; height++) {
-      for (int width = 0; width < matrix[height].length; width++) {
-        System.out.print(matrix[0][0] + "\t");
-      }
-      System.out.println();
-      }
-    }
-    return matrix();
-  }
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -1,16 +1,29 @@
+// RunMatrix.java
 import java.util.Scanner;
 
 public class RunMatrix {
-  public static void main(String[] args) {
-    Matrix size1;
-    size1 = new Matrix();
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the size of the matrix using a positive number: ");
+        int matrixSize = scanner.nextInt();
+        scanner.nextLine();
 
-    System.out.println("Please enter the size of the matrix using a positive number: ");
-    //size1 = scanner.nextLine();
-    System.out.println("Your matrix is " + size1 + " X " + size1);
-    System.out.println("Now printing matrix with default values...");
-    scanner.close();
+
+        System.out.println("Your matrix is" + matrixSize + " X " + matrixSize);
+        System.out.println("Now printing matrix with default values...");
+
+        Matrix size1 = new Matrix(matrixSize);
+
+        scanner.close();
     }
-  }
+}
 
+
+
+
+
+// I need line 10 somehow- how can I fix it- not taking user input
+// fill with numbers- use 1-(input*input)
+//highlight diagonal
+//swap numbers
+//print out flipped matrix
