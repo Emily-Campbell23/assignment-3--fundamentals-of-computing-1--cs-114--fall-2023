aa9418340975d88bc5585dd3ae1303df544c69b1
Emily-Campbell23 <emcampbel@hartford.edu>
2023-11-15 20:36:25 -0500

    added if else statement

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
-public class RunMatrix {
-    public static void main(String[] args) {
+public class RunMatrix{
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the size of the matrix: ");
-        //int matrixSize = scanner.nextInt();
-        scanner.nextLine();
-
-       // Matrix matrix = new Matrix(matrixSize);
-
+        System.out.print("Enter the size of the matrix with only positive numbers: ");
+        int size = scanner.nextInt();
+        if (size <= 0){
+            System.out.println("Invalid size. Please enter a positive number.");
+        }
+        else{
+            Matrix matrix = new Matrix(size);
+            matrix.printMatrix();
+        }
         scanner.close();
     }
 }
